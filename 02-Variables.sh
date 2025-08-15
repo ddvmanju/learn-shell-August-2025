43175cfa0d3c6ef35adee3f76d3e80aa84b9e98c
Master CVV <ddv.manju@gmail.com>
2025-08-15 15:47:26 +0530

    Learn shell variables

---
+++ 02-Variables.sh
@@ -0,0 +1,15 @@
+#if we assign a name to a set of data it is called variable
+
+#syntax : var_name=data
+
+x=10
+
+if we have any special characters in the RHS then we can provide that in quotes
+x1="abc 123"
+
+#access the variables
+#syntax: $var_name, or $(var_name)
+
+file=1.txt
+touch $file
+cp $file 2.txt
